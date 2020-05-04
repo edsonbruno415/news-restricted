c0055a2d5f10054cecda647c9e0f53a42016b823
Edson Bruno <edsonbruno415@gmail.com>
2020-05-04 18:42:09 -0300

    Add method for encrypting passwords

--- models/User.js
+++ models/User.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const UserSchema = mongoose.Schema({
+const getHash = async (password) => {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
+}
+
+const UserSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true
@@ -9,6 +16,10 @@ const UserSchema = mongoose.Schema({
         type: String,
         required: true
     }
+});
+
+UserSchema.pre('save', async function () {
+    this.password = await getHash(this.password);
 });
 
 const User = mongoose.model('User', UserSchema);
